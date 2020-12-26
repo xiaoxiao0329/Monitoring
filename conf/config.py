231f8a9f1ddbfabf672d7fb974b5402e0db013fd
Cherry.Tree <2454825342@qq.com>
2020-12-26 22:36:54 +0800

    Default Changelist

--- conf/config.py
+++ conf/config.py
@@ -1,10 +1,10 @@
-# 钉钉配置 keyname 设置钉钉的关键字
+# TODO:钉钉配置 keyname 设置钉钉的关键字
 webhook = ""
 keyName = ""
 
-# 设置硬盘监控上限 如 硬盘总和的 15% 写 0.15 默认为设置 85%
-# 此配置参数被 system/Disk.py 所引用
+# TODO:设置硬盘监控上限 如 硬盘总和的 15% 写 0.15 默认为设置 85%
+# TODO:此配置参数被 system/Disk.py 所引用
 threshold = 0.85
 
-# 设置监控程序的名 以列表形式写入逗号分割服务名
-server_name = ['', '', '', '']
+# TODO:设置监控程序的名 以列表形式写入逗号分割服务名
+server_name = ['']
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from conf import config
 from system import Disk
 from system import SysProcess
 
-# ================================================配置区=========================================
-# 以下配置区根据需求选择开启
+# ================================================ 配置区 ================================================
+# TODO:以下配置根据需求选择开启
 
 if __name__ == '__main__':
-    # 硬盘监控 -> 发送信息给钉钉
+    # TODO:硬盘监控
     Disk.ShowDisk(webhook=config.webhook, keyName=config.keyName).disk_info()
-    # 进程监控
+
+    # TODO:进程监控 建议与supervisor 进行使用 此功能不涉及进程重启 只监控状态
     SysProcess.check_server().checkprocess()
